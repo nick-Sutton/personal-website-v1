76d4ec14e60ca890daaeffab764ea42c1eca0d3a
nick-Sutton <136986806+nick-Sutton@users.noreply.github.com>
2024-06-01 21:10:17 -0400

    Altered about section

--- src/components/AboutTabs.js
+++ src/components/AboutTabs.js
@@ -13,7 +13,16 @@ const AboutTabs = () => {
                 className="mb-3"
             >
                 <Tab className="about-tab-button" eventKey="profile" title="Profile">
-                    Tab content for Home
+                    <div className="about-tab-content">
+                        <p>
+                            Hello, I'm Nick. I am a <b className="r">Computer Science</b> student, interested in <b className="g">Machine learning</b>, 
+                            <b className="y"> low-level-systems</b>, and <b className="b">Robotics</b>.
+                        </p>
+                        <p>
+                            My interest in computers started from a young by watching my brother play Super Mario 64 on an old CRT TV. 
+                            That facination with pologyons has blesomned into a lifelong love of technology and software.
+                        </p>
+                    </div>
                 </Tab>
                 <Tab className="about-tab-button" eventKey="education" title="Education">
                     Tab content for Profile
